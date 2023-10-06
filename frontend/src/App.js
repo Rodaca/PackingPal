377423c444e603a09d37ba2c8e3d0467aef2d185
Rodacay <leapardo4@gmail.com>
2023-10-05 21:50:41 -0500

    style: :lipstick: hace responsive y mochila

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import {BrowserRouter as Router,Route} from 'react-router-dom'
 
 import CategoriaRead from './components/categorias';
+import Mochila from './components/mochila';
 
 import Update_Ropa from './components/Ropa/update';
 import Create_Ropa from './components/Ropa/create';
@@ -26,14 +27,18 @@ import Update_Otros from './components/Otros/update';
 
 import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 
+
+
 function App() {
   return (
     <Router>
       <div className="App">
         <h1>PACKINGPAL</h1>
         <h2>Categorias</h2>
+        
         <CategoriaRead className=""/>
         <div className='main'>
+          <Mochila/>
           <div>
             <Route exact path="/ropa" component={Read_Delete_Ropa}></Route>
           </div>
---
+++ frontend/src/components/mochila.js
@@ -0,0 +1,12 @@
+import mochila from "../backpack.png";
+import React from 'react';
+
+export default function Mochila() {
+  return (
+    <div className="mochila">
+      
+      <img src={mochila} alt="Descripción de la imagen" />
+    </div>
+  );
+}
+
